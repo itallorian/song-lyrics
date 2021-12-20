19bdd0aa0b7ed022534efb6562b751afc4c4b62a
Ítallo Rian <itallo.rian@outlook.com>
2021-12-20 12:27:25 -0300

    [feature] método para adição de lyricsearch

--- src/LYRICS.INTEGRATION.REPOSITORY/Repositories/Intefaces/Integration/ILyricsSearchRepository.cs
+++ src/LYRICS.INTEGRATION.REPOSITORY/Repositories/Intefaces/Integration/ILyricsSearchRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace LYRICS.INTEGRATION.REPOSITORY.Repositories.Intefaces.Integration
+﻿using LYRICS.INTEGRATION.BUSINESSLOGIC.Models.Integration;
+
+namespace LYRICS.INTEGRATION.REPOSITORY.Repositories.Intefaces.Integration
 {
     public interface ILyricsSearchRepository
     {
+        decimal AddLyricSearch(LyricsSearch model);
     }
 }
--- src/LYRICS.INTEGRATION.REPOSITORY/Repositories/Integration/LyricsSearchRepository.cs
+++ src/LYRICS.INTEGRATION.REPOSITORY/Repositories/Integration/LyricsSearchRepository.cs
@@ -1,9 +1,38 @@
-﻿using LYRICS.INTEGRATION.REPOSITORY.Repositories.Intefaces.Integration;
+﻿using LYRICS.INTEGRATION.BUSINESSLOGIC.Models.Integration;
+using LYRICS.INTEGRATION.REPOSITORY.Context;
+using LYRICS.INTEGRATION.REPOSITORY.Models.Integration.Tables;
+using LYRICS.INTEGRATION.REPOSITORY.Repositories.Intefaces.Integration;
 
 namespace LYRICS.INTEGRATION.REPOSITORY.Repositories.Integration
 {
     public class LyricsSearchRepository : ILyricsSearchRepository
     {
         public LyricsSearchRepository() { }
+
+        public decimal AddLyricSearch(LyricsSearch model)
+        {
+            try
+            {
+                using (var context = new IntegrationContext())
+                {
+                    var lyricSearch = new TB_LYRICS_SEARCH()
+                    {
+                        INSERTION_DATE = DateTime.Now,
+                        AUTHOR = model.Author,
+                        TITLE = model.Title,
+                        VALID = model.Valid
+                    };
+
+                    context.Add(lyricSearch);
+                    var ret = context.SaveChanges();
+
+                    return lyricSearch.LYRIC_SEARCH;
+                }
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
     }
 }
